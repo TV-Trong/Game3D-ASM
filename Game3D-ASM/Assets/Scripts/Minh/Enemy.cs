807b47a53939fddf2f8243b811b6daabd2cba6d5
nminh123 <caonhatminh245@gmail.com>
2024-12-02 08:59:55 +0700

    Terrain & Add package Enemy, scripts,....

---
+++ Game3D-ASM/Assets/Scripts/Minh/Enemy.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private Transform spawnPos;
+    [SerializeField] private Transform deSpawnPos;
+
+    
+}
