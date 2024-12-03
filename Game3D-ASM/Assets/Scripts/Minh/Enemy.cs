dc967b186938b7a40bdb2399edaa152e354b1bf0
nminh123 <caonhatminh245@gmail.com>
2024-12-04 01:16:32 +0700

    Thieu UI, animation

--- Game3D-ASM/Assets/Scripts/Minh/Enemy.cs
+++ Game3D-ASM/Assets/Scripts/Minh/Enemy.cs
@@ -5,13 +5,17 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] private float speed = 5;
     private PlayerBehaviour mPlayerBehavior;
+    private GameObject mPlayer;
     private BoxCollider mCollision;
+    private bool isAttacking;
+    private Animator mAnimator;
 
     void Awake()
     {
         try
         {
             mPlayerBehavior = FindObjectOfType<PlayerBehaviour>();
+            mPlayer = GameObject.Find("Asuna");
         }
         catch (System.Exception)
         {
@@ -20,16 +24,37 @@ public class Enemy : MonoBehaviour
         }
 
         mCollision = GetComponent<BoxCollider>();
+        mAnimator = GetComponent<Animator>();
     }
 
     void Update()
     {
         ChasingPlayer();
+        CheckDistancePlayer();
     }
 
     void ChasingPlayer()
     {
-        this.transform.position = Vector3.MoveTowards(this.transform.position, mPlayerBehavior.transform.position, speed * Time.deltaTime);   
+        Vector3 direction = mPlayerBehavior.transform.position - transform.position;
+        direction.y = 0; // Giữ hướng trên mặt phẳng (x, z), không ảnh hưởng bởi trục y
+        transform.rotation = Quaternion.LookRotation(direction);
+
+        // Di chuyển nhân vật tới mPlayer
+        transform.position = Vector3.MoveTowards(transform.position, mPlayerBehavior.transform.position, speed * Time.deltaTime);
+    }
+
+    void CheckDistancePlayer()
+    {
+        if (Vector3.Distance(this.transform.position, mPlayer.transform.position) == .5f)
+        {
+            isAttacking = true;
+            mAnimator.SetBool("isAttacking", isAttacking);
+        }
+        else
+        {
+            isAttacking = false;
+            mAnimator.SetBool("isAttacking", isAttacking);
+        }
     }
 
     void OnTriggerEnter(Collider other)
